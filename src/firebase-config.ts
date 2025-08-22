c1b4465cedf48dfeae6946aa7720e6ebc5632dbd
Raghunandan0 <amny0843@gmail.com>
2025-08-23 01:45:21 +0530

    Refactor Firebase config to use environment variables

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -21,20 +21,17 @@
 
 
 
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";       // <-- Add this
-import { getFirestore } from "firebase/firestore"; // <-- And this
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDb7c8j-fm4n5OqYFxVrRJjvJNo9ifNktg",
-  authDomain: "docs-clone-8141d.firebaseapp.com",
-  projectId: "docs-clone-8141d",
-  storageBucket: "docs-clone-8141d.appspot.com",
-  messagingSenderId: "988634929035",
-  appId: "1:988634929035:web:39ba6f25f2f07a7482c18e"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
